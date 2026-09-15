0ea96670350b25631ff2d833655ad3f86e2fd3a6
kaede050492 <tamu1296tt@gmail.com>
2026-09-16 03:20:29 +0900

    fix: publish legacy compatibility file

--- manifest.lua
+++ manifest.lua
@@ -7,7 +7,7 @@ return {
     build = 1400,
     edition = "Tom's Peripherals",
     channel = "stable",
-    legacy = "hccos.v1.3.3.lua",
+    legacy = "hccos_v1.3.3.lua",
     files = {
         "manifest.lua",
         "version.lua",
@@ -51,3 +51,4 @@ return {
         "apps/recovery.lua"
     }
 }
+
